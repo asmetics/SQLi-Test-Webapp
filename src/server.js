6ab31918a1145d4b966aff7e2aa09daee0c6dae2
asmetics <nuqko@proton.me>
2025-06-20 14:12:34 -0500

    added docker

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
+const port = 3000
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "..", "client", "entry")));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "entry", "index.html"));
 });
 
-app.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+app.listen(port, "127.0.0.1", () => {
+  console.log("Listening on http://127.0.0.1:" + port);
 });
